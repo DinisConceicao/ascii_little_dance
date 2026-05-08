977392b200bd9ed5f0d211c4fd803ed269209464
punpumpensumpeeuuum <dinisconceicao2004@gmail.com>
2026-05-08 04:34:54 +0100

    passinho 3

--- server.py
+++ server.py
@@ -1,22 +1,25 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import os
-import time
 
-FRAMES_DIR = "ascii_frames"
-frames = sorted(os.listdir(FRAMES_DIR))
+FRAMES = sorted(os.listdir("ascii_frames"))
 
 class Handler(BaseHTTPRequestHandler):
 	def do_GET(self):
-		self.send_response(200)
-		self.send_header("Content-type", "text/plain")
-		self.end_headers()
+		path = self.path
 
-		while True:
-			for f in frames:
-				with open(f"{FRAMES_DIR}/{f}", "r") as file:
-					self.wfile.write(b"\033[2J\033[H")
-					self.wfile.write(file.read().encode())
-					self.wfile.flush()
-				time.sleep(0.05)
+		if path == "/dance":
+			self.send_response(200)
+			self.end_headers()
+			self.wfile.write("\n".join(FRAMES).encode())
+
+		elif path.startswith("/dance/"):
+			idx = int(path.split("/")[-1])
+			frame = FRAMES[idx % len(FRAMES)]
+
+			self.send_response(200)
+			self.end_headers()
+
+			with open(f"ascii_frames/{frame}") as f:
+				self.wfile.write(f.read().encode())
 
 HTTPServer(("0.0.0.0", int(os.environ.get("PORT", 8080))), Handler).serve_forever()
